453fbcca8d5f21a0aaab9a6f552cf73a5fd7cb52
Cody House <codyahouse@gmail.com>
2014-10-21 12:23:12 -0400

    Uploading Code for assignment 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+tip_amount = (55 * 0.15)
+
+puts "A good tip for a 55$ dollar meal would be #{tip_amount}"
+
+puts "I am " + 24.to_s + " years old"
+
+sum_of = 45628 * 7839
+
+ puts "The Sum is #{sum_of}"
+
+ puts "(true && false) || (false && true) || !(false && false) == true"
