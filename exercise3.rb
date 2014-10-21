78b1e26c6873af4ccf2326414a5441acd77a33c7
Cody House <codyahouse@gmail.com>
2014-10-21 12:34:21 -0400

    Added Greeting and the year they were born

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,10 @@
 puts "What is your name?"
+name = gets.chomp.strip
+
+puts "Bonjour, #{name}"
+
+puts "How old are you, friend?"
+
+year_born = 2014 - gets.chomp.to_i
+
+puts "You were born in the year #{year_born}"
